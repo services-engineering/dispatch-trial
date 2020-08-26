ed2233fd99a37ac5fa306a8965a407068d4a84ae
Eric Goode <egoode@mesosphere.com>
2020-08-26 14:48:23 -0500

    Trying something new

--- main.go
+++ main.go
@@ -1,11 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+var Version string
+var Buildtime string
 
 func main() {
-    fmt.Printf("Hello %v!", World())
+	go func() {
+		for range time.NewTicker(time.Second).C {
+			log.Printf("This is a heartbeat..." + time.Now().String())
+		}
+	}()
+	// use PORT environment variable, or default to 8080
+	port := "8080"
+	if fromEnv := os.Getenv("PORT"); fromEnv != "" {
+		port = fromEnv
+	}
+
+	// register hello function to handle all requests
+	server := http.NewServeMux()
+	server.HandleFunc("/", hello)
+
+	// start the web server on port and accept requests
+	log.Printf("Server listening on port %s", port)
+	err := http.ListenAndServe(":"+port, server)
+	log.Fatal(err)
 }
 
-func World() string {
-    return "World"
+// hello responds to the request with a plain-text "Hello, world" message.
+func hello(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Serving request: %s", r.URL.Path)
+	fmt.Fprintf(w, "Hello, world!\n")
+	fmt.Fprintf(w, "Version: %s\n", Version)
+	fmt.Fprintf(w, "Build time: %s\n", Buildtime)
+	fmt.Fprintf(w, "D2iQ CI/CD!\n")
 }
